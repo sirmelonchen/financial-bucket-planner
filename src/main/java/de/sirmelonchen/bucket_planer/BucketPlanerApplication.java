ef315084ca7e5e491bd0943d94290287751fa4ee
sir_melonchen <beug.kon@outlook.de>
2025-05-19 20:59:06 +0200

    feat: Update Gradle wrapper and project settings
    This commit updates the Gradle wrapper to version 8.13,
    adds SPDX license identifiers to the wrapper scripts,
    improves error handling in the wrapper scripts, and
    updates the .gitignore file to include more
    IntelliJ IDEA and STS files.  Additionally, it
    configures network timeout and distribution URL
    validation for the Gradle wrapper. (main)

---
+++ src/main/java/de/sirmelonchen/bucket_planer/BucketPlanerApplication.java
@@ -0,0 +1,13 @@
+package de.sirmelonchen.bucket_planer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BucketPlanerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BucketPlanerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/sirmelonchen/bucket_planer/BucketPlanerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.sirmelonchen.bucket_planer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BucketPlanerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
